ab41f816a7c30f938d5a2a4e5d02f3b4b2a77c9d
PJ <spyder3k@users.noreply.github.com>
2015-09-16 17:34:24 -0500

    Functions body
    Adding body of functions.

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,33 @@
-## Put comments here that give an overall description of what your
-## functions do
+## DataScience practice 
+## Assignament 2. Inverse of a Matrix
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+## This function is a "list" of operations availables
+makeCacheMatrix <- function(mtx = matrix()) {
+    # intializing variables
+    inv <- NULL
+    set <- function(param) {
+        mtx <<- param
+        inv <<- NULL
+    }
+    
+    get <- function() x
+    setInvMatrix <- function(inverse) inv <<- inverse
+    getInvMatrix <- function() inv
+    # "List" of operations or function availables
+    list(set=set, get=get, setInvMatrix=setInvMatrix, getInvMatrix=getInvMatrix)
 }
 
 
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+## This function verify if an operation has been executed
+cacheSolve <- function(param, ...) {
+    inv <- param$getInvMatrix()
+    # operation has been executed?
+    if(!is.null(inv)) {
+        message("Your data has been cached.")
+        return(inv)
+    }
+    data <- param$get()
+    inv <- solve(data)
+    param$setInvMatrix(inv)
+    inv
 }
